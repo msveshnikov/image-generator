99651cea2ff121a6f7221dad14d0903f14556ce4
Max Sveshnikov <msveshnikov@gmail.com>
2022-09-10 17:23:59 +0200

    ligthbox

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import PhotoGallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
 const API_URL = "https://mega.maxsoft.tk";
 
@@ -16,12 +17,42 @@ export async function getPhotos() {
 
 function App() {
     const [photos, setPhotos] = useState([]);
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
 
     useEffect(() => {
         getPhotos().then(setPhotos);
     }, []);
 
-    return <PhotoGallery photos={photos} />;
+    return (
+        <>
+            <PhotoGallery photos={photos} onClick={openLightbox} />
+            <ModalGateway>
+                {viewerIsOpen && (
+                    <Modal onClose={closeLightbox}>
+                        <Carousel
+                            currentIndex={currentImage}
+                            views={photos.map((x) => ({
+                                ...x,
+                                srcset: x.srcSet,
+                                caption: x.title,
+                            }))}
+                        />
+                    </Modal>
+                )}
+            </ModalGateway>
+        </>
+    );
 }
 
 export default App;
